1b71925e4ac4ae1329cac475c4c55abbc835e79e
Yash Kadam <yash@Yashs-MacBook-Air.local>
2024-10-19 21:52:17 +0530

    solved gameofpig

--- gameofpig/cmd/root.go
+++ gameofpig/cmd/root.go
@@ -4,11 +4,11 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"errors"
 	"fmt"
 	"math/rand"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -29,20 +29,103 @@ to quickly create a Cobra application.`,
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
 		// validate args to be int
-		p1s, p2s, err := validateArguments(args)
-		if err != nil {
-			fmt.Println(err)
+		p1s, p2s := args[0], args[1]
+
+		if strings.Contains(args[0], "-") && strings.Contains(args[1], "-") {
+			variableVsvariableStrategey(p1s, p2s)
+			return
 		}
-		totalGames := 10
-		p1Win := 0
-		for i := 0; i < totalGames; i++ {
-			winner := playPig(p1s, p2s)
-			if winner == "p1" {
-				p1Win += 1
-			}
+		p1HoldScore, err := strconv.Atoi(p1s)
+		if err != nil || p1HoldScore > 100 || p1HoldScore < 1 {
+			fmt.Println("invalid strategy for Player 1")
 		}
-		fmt.Println("Player 1 won", p1Win, "games")
+		if strings.Contains(p2s, "-") {
+			fixedVsVariableStrategy(p1HoldScore, p2s)
+			return
+		}
+		p2HoldScore, err := strconv.Atoi(p2s)
+		if err != nil || p2HoldScore > 100 || p2HoldScore < 1 {
+			fmt.Println("invalid strategy for player 2")
+		}
+		p1Win, totalGames := fixedVsfixedStrategy(p1HoldScore, p2HoldScore)
+
+		losses := totalGames - p1Win
+		fmt.Printf(
+			"Holding at %d vs Holding at %d: wins: %d/%d (%.2f%%), losses: %d/%d (%.2f%%)\n",
+			p1HoldScore, p2HoldScore, p1Win, totalGames, (float64(p1Win)/float64(totalGames))*100,
+			losses, totalGames, (float64(losses)/float64(totalGames))*100,
+		)
 	},
+}
+
+func variableVsvariableStrategey(p1s, p2s string) {
+	p1start, p1end := getRange(p1s)
+	p2start, p2end := getRange(p2s)
+	for i := p1start; i <= p1end; i++ {
+		p1TotalWin := 0
+		totalGames := 0
+		for j := p2start; j <= p2end; j++ {
+			if i == j {
+				continue
+			}
+			p1Win, totalGame := fixedVsfixedStrategy(i, j)
+			p1TotalWin += p1Win
+			totalGames += totalGame
+		}
+		losses := totalGames - p1TotalWin
+		fmt.Printf("Result: Wins, losess staying at k = %d: %d/%d (%.2f%%), %d/%d (%.2f%%)\n",
+			i, p1TotalWin, totalGames, (float32(p1TotalWin)/float32(totalGames))*100,
+			losses, totalGames, (float32(losses)/float32(totalGames))*100,
+		)
+	}
+}
+
+func fixedVsVariableStrategy(p1s int, p2s string) {
+	start, end := getRange(p2s)
+	for i := start; i <= end; i++ {
+		if p1s == i {
+			continue
+		}
+		p1Win, totalGames := fixedVsfixedStrategy(p1s, i)
+		losses := totalGames - p1Win
+		fmt.Printf(
+			"Holding at %d vs Holding at %d: wins: %d/%d (%.2f%%), losses: %d/%d (%.2f%%)\n",
+			p1s, i, p1Win, totalGames, (float64(p1Win)/float64(totalGames))*100,
+			losses, totalGames, (float64(losses)/float64(totalGames))*100,
+		)
+
+	}
+}
+
+func getRange(rangeArg string) (int, int) {
+	rangeArray := strings.Split(rangeArg, "-")
+	if len(rangeArray) > 2 {
+		fmt.Println("incorrect range, should be of format 1-100")
+	}
+	start, err := strconv.Atoi(rangeArray[0])
+	if err != nil || start < 1 || start > 100 {
+		fmt.Println("invalid strategy range")
+	}
+	end, err := strconv.Atoi(rangeArray[1])
+	if err != nil || end < 1 || end > 100 {
+		fmt.Println("invalid strategy range")
+	}
+	if start > end {
+		fmt.Println("invalid strategy range")
+	}
+	return start, end
+}
+
+func fixedVsfixedStrategy(p1s, p2s int) (int, int) {
+	totalGames := 10
+	p1Win := 0
+	for i := 0; i < totalGames; i++ {
+		winner := playPig(p1s, p2s)
+		if winner == "p1" {
+			p1Win += 1
+		}
+	}
+	return p1Win, totalGames
 }
 
 func playPig(p1s, p2s int) string {
@@ -73,10 +156,8 @@ func playPig(p1s, p2s int) string {
 		}
 	}
 	if p1Score >= 100 {
-		fmt.Println("Player 1 final Score is ", p1Score, "and Player 2 final score is ", p2Score)
 		return "p1"
 	} else {
-		fmt.Println("Player 1 final Score is ", p1Score, "and Player 2 final score is ", p2Score)
 		return "p2"
 	}
 }
@@ -90,22 +171,12 @@ func playTurn(playerStrategy int) int {
 	score := 0
 	for score < playerStrategy {
 		roll := rollDie()
-		fmt.Println("The die rolled", roll)
 		if roll == 1 {
 			return 0
 		}
 		score += roll
 	}
 	return score
-}
-
-func validateArguments(args []string) (p1Strategy int, p2Strategy int, err error) {
-	p1Strategy, err1 := strconv.Atoi(args[0])
-	p2Strategy, err2 := strconv.Atoi(args[1])
-	if err1 != nil && err2 != nil {
-		return 0, 0, errors.New("invalid arguments")
-	}
-	return p1Strategy, p2Strategy, nil
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
